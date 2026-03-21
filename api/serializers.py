4a8536bb55cf88e132d42763ebeec81b9b61a7a6
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-03-21 23:43:30 +0100

    Serializers for user model

---
+++ api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'status', 'created_at', 'updated_at']
+        read_only_fields = ['user']
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
